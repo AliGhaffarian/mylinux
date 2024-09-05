8a2ef694ecc2acca81b7fffb99c58fd2e04fa576
AliGhaffarian <alighaffarian9@gmail.com>
2024-09-05 18:56:43 +0330

    05_09_2024:18:53

--- Documents/startup/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mouse_id=$(xinput list | grep "ASUSTek TUF GAMING M5" | grep -v Keyboard | grep -o -E "id=[[:digit:]]+" | cut -f 2 -d =)
-xinput set-prop  $mouse_id "libinput Accel Speed" -1
