4da32acc53b8d7c0f16cdffa43705a76b649475f
AliGhaffarian <alighaffarian9@gmail.com>
2024-10-25 22:04:46 +0330

    25_10_2024:22:03

--- Documents/startup/lip.sh
+++
@@ -1,7 +0,0 @@
-a=$(ip r|head -n 1|grep wlp4s0)
-gip=$(echo $a|awk '{print $3}'|cut -d. -f3,4)
-lip=$(echo $a|awk '{print $9}'|cut -d. -f3,4)
-if [[ $lip != "" ]] then
-    echo "%{F#00FFFF}Gate%{F-} $gip %{F#00FFFF}Ip%{F-} $lip"
-else echo;
-fi
