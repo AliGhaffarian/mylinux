a8276e72d259d5127324d38b794e2c273eaacd91
AliGhaffarian <alighaffarian9@gmail.com>
2024-11-14 19:56:01 +0330

    14_11_2024:19:54

---
+++ Documents/startup/alias_files/update_joke.sh
@@ -0,0 +1,9 @@
+joke=$(curl https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,explicit\&format=txt)
+joke_file=/home/user/Documents/startup/alias_files/joke
+
+if [ -z "$joke"];then
+		exit
+fi
+
+echo "$joke" > $joke_file
+
