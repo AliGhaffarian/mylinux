eedfb158643b43d4e1fb86145f36ec8d2f420791
AliGhaffarian <alighaffarian9@gmail.com>
2025-01-01 17:34:39 +0330

    01_01_2025:17:32

---
+++ Documents/linux_build_init/installs/neovim/setup_neovim.sh
@@ -0,0 +1,20 @@
+# shellcheck source=../deps/abs.lib.depcheck
+source abs.lib.depcheck
+
+conf_repo="AliGhaffarian/nvim_conf"
+
+function download_and_cp() {
+  tmpdir=$(mktemp -d)
+  # download the latest zip file of repo in the /tmp then extract it.
+  url="https://github.com/$conf_repo/archive/refs/heads/main.zip"
+  curl -L $url -o /tmp/$tmpdir/nvim_conf.zip 1>/dev/null 2> /dev/null
+  unzip /tmp/$tmpdir/nvim_conf.zip -d /tmp/extract_nvim_conf 1>/dev/null 2> /dev/null
+  cp -rf /tmp/$tmpdir/extract_nvim_conf/* ~
+}
+
+
+if ! depcheck_cmd "./requirements_cmd_vim.txt"; then
+		exit 1
+fi
+
+download_and_cp
